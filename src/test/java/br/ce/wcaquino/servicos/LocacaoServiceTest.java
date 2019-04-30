d44ae3ce2ed7712bf72e06aebba21385bc23f463
Robert Junio <rjmonteiro@indracompany.com>
2019-04-30 16:58:16 -0300

    Aula 09 - Tratatemento de exceçoes - parte 1

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -9,18 +9,23 @@ import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() throws Exception {
+		
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
@@ -28,15 +33,56 @@ public class LocacaoServiceTest {
 		
 		//acao
 		Locacao locacao = service.alugarFilme(usuario, filme);
-		
+			
 		//verificacao
-		Assert.assertEquals(5.0, locacao.getValor(), 0.01);
+		Assert.assertEquals("Erro na linha 33 ", 5.0, locacao.getValor(), 0.01); //comentario para facilitar a localização do erro
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 		
 		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(5.0))); //verifique que
-		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(5.0)));
+		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(4.0)));
 		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(false)); //aqui foi realizado o importe estatico
+		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(3)), is(false)); //aqui foi realizado o importe estatico
+		
+	}
+	
+	@Test(expected = Exception.class) // teste para tratar excessão esperada - modelo 1
+	public void testeFilmeSemEstoque() throws Exception {
+		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		service.alugarFilme(usuario, filme);
+	}
+	
+	@Test // teste para tratar excessão esperada - modelo 2
+	public void testeFilmeSemEstoque2() throws Exception {
+		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		try {
+			service.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter uma exceção");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme sem estoque")); 
+		}
+		
+	}
+	
+	@Test // teste para tratar excessão esperada - modelo 3
+	public void testeFilmeSemEstoque3() throws Exception {
+		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+		
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+
+		service.alugarFilme(usuario, filme);
+		
 	}
 }
