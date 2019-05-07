267d420c7b94059f48154bb7e305e31c143465a3
Robert Junio <rjmonteiro@indracompany.com>
2019-05-07 15:18:12 -0300

    Aula 34 - Spy

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -3,18 +3,37 @@ package br.ce.wcaquino.servicos;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import br.ce.wcaquino.exceptions.DividirPorZeroException;
 
 public class CalculadoraTest {
 	
 	private Calculadora calc;
+	@Mock
+	private Calculadora calcMock;
+	@Spy
+	private Calculadora calcSpy;
 	
 	@Before
 	public void setup() {
 		calc = new Calculadora();
+		MockitoAnnotations.initMocks(this);
 	}
 
+	@Test
+	public void testeDiferencaMockSpy() {
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(8);
+		//Mockito.when(calcMock.somar(1, 2)).thenCallRealMethod(); //para o mock também executar o metodo
+		Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+		
+		System.out.println("Mock: " + calcMock.somar(1, 5)); //quando nao sabe o que fazer volta o valor padrao
+		System.out.println("Spy: " + calcSpy.somar(1, 5)); //quando nao sabe o que fazer ele executa o metodo
+	}
+	
 	@Test
 	public void testeSomaDoisValores() {
 
